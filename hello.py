dab7e74c824e02623b2715615c9755a6860f990e
teodorauksanovic <teodora.uksanovic@udg.edu.me>
2019-02-11 15:09:20 +0100

    Add file to github

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
+def validnibrojevi(x,y):
+    if not x.isnumeric() or not y.isnumeric():
+        return False
+    else:
+        return True
+
+
 korisnikunioprvi = input("unesiteprvibroj:")
 korisnikuniodrugi = input("unesitedrugibroj:")
 
 
-if (not korisnikunioprvi.isnumeric()) or (not korisnikuniodrugi.isnumeric()):
-    print("niste unijeli broj")
-    quit()
-
+if not validnibrojevi(korisnikunioprvi, korisnikuniodrugi):
+    quit("brojevi nisu validni")
 
 brojprvi = int(korisnikunioprvi)
 brojdrugi = int(korisnikuniodrugi)
@@ -14,4 +19,3 @@ brojdrugi = int(korisnikuniodrugi)
 
 zbir = brojprvi + brojdrugi
 print(zbir)
-
